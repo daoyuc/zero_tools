e00a549c331faf0219cd36a425ebd24f7c8e3585
mouc <chzmyf@gmail.com>
2022-03-31 17:14:53 +0800

    opt wp_posts comment_count

--- app/Commands/BbCommand.php
+++ app/Commands/BbCommand.php
@@ -13,7 +13,7 @@ class BbCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'bb {content?} {--p}';
+    protected $signature = 'bb {content?} {--p}  {--e}';
 
     /**
      * The description of the command.
@@ -35,33 +35,73 @@ class BbCommand extends Command
         //dd($content, $p);
         //^ null
         //^ false
-        $comment_post_ID = 109; //定义你的说说页面ID
-        $comment_author = 'pwa';
-        $user_id = 1;
+        $comment_post_ID = config('bb.post_id'); //定义你的说说页面ID
+
+        if (!$comment_post_ID) {
+            $this->error('config bb.post_id is null!');
+            exit;
+        }
+        $comment_author = config('bb.author');
+        $user_id = config('bb.user_id');
+        $comment_author_email = config('bb.email');
 
         if (!$content) {
             //查询评论
 
             $comments = DB::table('wp_comments')->select('comment_content', 'comment_date')
-            ->where('comment_post_ID', $comment_post_ID)->orderBy('comment_ID', 'DESC')->get(5)
-            ->map(function ($row) {
-                return [
-                    $this->transContent($row->comment_content), $row->comment_date,
-                ];
-            });
+                ->where('comment_post_ID', $comment_post_ID)
+                ->orderBy('comment_ID', 'DESC')
+                ->limit(config('bb.num'))
+                ->get()
+                ->map(function ($row) {
+                    return [
+                        $this->transContent($row->comment_content), $row->comment_date,
+                    ];
+                });
 
             //dd($comments);
             $headers = ['乱弹', '时间'];
             $this->table($headers, $comments); //borderless, 'compact'
         } else {
+            if ($this->option('e')) {
+                //修改上一条
+                $affected = DB::update(
+                    'UPDATE wp_comments SET comment_content = ? WHERE comment_post_ID = ? AND comment_approved = 1 ORDER BY comment_id DESC LIMIT 1',
+                    [$content, $comment_post_ID]
+                );
+                if ($affected) {
+                    $this->info('edit done');
+                    exit;
+                } else {
+                    $this->error('edit ooops!');
+                }
+            }
             $inSql = 'INSERT INTO wp_comments
-            (comment_post_ID, comment_author,comment_date, comment_date_gmt,comment_content,comment_agent,`user_id`)
-            VALUES (?,?,?,?,?,?,?)';
+            (comment_post_ID,
+            comment_author,
+            comment_author_email,
+            comment_date,
+            comment_date_gmt,
+            comment_content,
+            comment_agent,
+            `user_id`)
+            VALUES (?,?,?,?,?,?,?,?)';
             $newId = DB::insert($inSql, [
-                $comment_post_ID, $comment_author, gmdate('Y-m-d H:i:s', time() + 8 * 3600), date('Y-m-d H:i:s'), $content, 'cmd', $user_id,
+                $comment_post_ID,
+                $comment_author,
+                $comment_author_email,
+                gmdate('Y-m-d H:i:s', time() + 8 * 3600),
+                date('Y-m-d H:i:s'),
+                $content,
+                'cmd',
+                $user_id,
             ]);
             if ($newId) {
                 $this->info('done');
+                DB::update(
+                    'UPDATE wp_posts SET comment_count =(SELECT COUNT(*) FROM wp_comments WHERE comment_post_ID = ? AND comment_approved = 1) WHERE ID = ?',
+                    [$comment_post_ID, $comment_post_ID]
+                );
             } else {
                 $this->error('ooops!');
             }
